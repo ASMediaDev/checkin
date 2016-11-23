4e77d9aa2092a34e14330a8360e79333c954adfe
Alex Seitz <alexanderseitz@macbook-2.local>
2016-11-23 10:48:21 +0100

    update..

---
+++ TicketVal/DBViewController.swift
@@ -0,0 +1,196 @@
+//
+//  DBViewController.swift
+//  TicketVal
+//
+//  Created by Alex Seitz on 21.11.16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class DBViewController: UIViewController {
+    
+    @IBOutlet weak var count: UITextView!
+    
+    @IBAction func importTickets(_ sender: Any) {
+        insert()
+    }
+    @IBAction func truncate(_ sender: UIButton) {
+        truncateCoreData()
+    }
+    
+    var codes = [NSManagedObject]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countcodes()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   func saveCode (_ code: String){
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Tickets", in: managedContext)
+        
+        let ticket = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        ticket.setValue(code, forKey: "code")
+        
+        do{
+            try managedContext.save()
+            
+            codes.append(ticket)
+            
+        } catch let error as NSError{
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    func insert () {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let contentsOfUrl = Bundle.main.url(forResource: "Midsemester_Tickets_2", withExtension: "csv")
+        
+        let items = appDelegate.parseCSV(contentsOfUrl!, encoding: String.Encoding.utf8)
+        
+        var counter = 0
+        
+        for element in items! {
+            saveCode(element)
+            print ("\(element) inserted")
+            counter = counter + 1
+        }
+        
+        print("Import complete")
+        print("\(counter) items imported")
+        countcodes()
+        
+        let alert = UIAlertController(title: "Import erfolgreich", message: "\(counter) Tickets importiert", preferredStyle: UIAlertControllerStyle.alert)
+        let backView = alert.view.subviews.last?.subviews.last
+        backView?.layer.cornerRadius = 10.0
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func truncateCoreData(){
+        
+        let appDel = UIApplication.shared.delegate as! AppDelegate
+        let context = appDel.managedObjectContext
+        let coord = appDel.persistentStoreCoordinator
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Tickets")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            try coord.execute(deleteRequest, with: context)
+        } catch let error as NSError {
+            debugPrint(error)
+        }
+        
+        print("Coredata truncated")
+        countcodes()
+        let alert = UIAlertController(title: "Leeren erfolgreich", message: "Alle Tickets wurden aus der Datenbank gelöscht", preferredStyle: UIAlertControllerStyle.alert)
+        let backView = alert.view.subviews.last?.subviews.last
+        backView?.layer.cornerRadius = 10.0
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
+        func validate(_ scan: String) -> Bool{
+            
+        var valid = false
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Tickets")
+        
+        let predicate = NSPredicate(format: "code == %@", scan)
+        
+        fetchRequest.predicate = predicate
+        
+        do{
+            let results = try managedContext.fetch(fetchRequest)
+            
+            codes = results as! [NSManagedObject]
+            
+            //print("Codes:")
+            //print(codes[0])
+            
+            if results.isEmpty{
+                print("invalid")
+                valid = false
+            }else{
+                print("valid")
+                valid = true
+            }
+        
+        } catch let error as NSError{
+            print("Could not fetch \(error), \(error.userInfo)")
+            
+        }
+            return valid
+    }
+    
+    func countcodes(){
+    
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Tickets")
+        
+        
+        //fetchRequest.predicate = predicate
+        
+        do{
+            let results = try managedContext.fetch(fetchRequest)
+            
+            codes = results as! [NSManagedObject]
+            
+            count.text = "Es befinden sich \(codes.count) Tickets in der Datenbank"
+            
+            
+        } catch let error as NSError{
+            print("Could not fetch \(error), \(error.userInfo)")
+            
+        }
+    
+    }
+
+        
+        
+        
+        
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ TicketVal/MainViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MainViewController.swift
+//  TicketVal
+//
+//  Created by Alex on 16.11.16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let gradientLayer = CAGradientLayer()
+
+    @IBOutlet weak var scan: UIButton!
+    
+    @IBOutlet weak var database: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let topcolor = UIColor(red:0.90, green:0.62, blue:0.01, alpha:1.0)
+        let bottomcolor = UIColor(red:1.00, green:0.45, blue:0.00, alpha:1.0)
+        
+        let gradientColors: [CGColor] = [topcolor.cgColor, bottomcolor.cgColor]
+        let gradientLocations : [Float] = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        scan.layer.cornerRadius = 10.0
+        database.layer.cornerRadius = 10.0
+  
+        
+        print("landingpage")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
