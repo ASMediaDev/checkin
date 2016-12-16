47524c0f2aceb061beb9573460f7b62dd3fc60d4
Alex Seitz <alexanderseitz@macbook.fritz.box>
2016-12-16 20:53:23 +0100

    Replaced QRcode Library and added checkin feature

--- TicketVal/ScanViewController.swift
+++ TicketVal/ScanViewController.swift
@@ -12,147 +12,153 @@ import AVFoundation
 
 class ScanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
-    @IBOutlet weak var messageLabel: UILabel!
+
     
-    @IBAction func light(_ sender: Any) {
-        toggleFlash()
-    }
-    
-    var captureSession:AVCaptureSession?
-    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
-    var qrCodeFrameView:UIView?
-    
-    // Added to support different barcodes
-    let supportedBarCodes = [AVMetadataObjectTypeQRCode, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeAztecCode]
+    @IBOutlet weak var displaycode: UILabel!
+   
+   
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        displaycode.backgroundColor = UIColor.red
+        displaycode.text = "NO QR-CODE DETECTED"
         
-        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
-        // as the media type parameter.
-        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+        
+        let videoCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let videoInput: AVCaptureDeviceInput
         
         do {
-            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            
-            // Initialize the captureSession object.
-            captureSession = AVCaptureSession()
-            // Set the input device on the capture session.
-            captureSession?.addInput(input)
-            
-            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
-            let captureMetadataOutput = AVCaptureMetadataOutput()
-            captureSession?.addOutput(captureMetadataOutput)
-            
-            // Set delegate and use the default dispatch queue to execute the call back
-            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            
-            // Detect all the supported bar code
-            captureMetadataOutput.metadataObjectTypes = supportedBarCodes
-            
-            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-            videoPreviewLayer?.frame = view.layer.bounds
-            view.layer.addSublayer(videoPreviewLayer!)
-            
-            // Start video capture
-            captureSession?.startRunning()
-            
-            // Move the message label to the top view
-            view.bringSubview(toFront: messageLabel)
-            
-            // Initialize QR Code Frame to highlight the QR code
-            qrCodeFrameView = UIView()
-            
-            if let qrCodeFrameView = qrCodeFrameView {
-                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
-                qrCodeFrameView.layer.borderWidth = 2
-                view.addSubview(qrCodeFrameView)
-                view.bringSubview(toFront: qrCodeFrameView)
-            }
-            
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
-            // If any error occurs, simply print it out and don't continue any more.
-            print(error)
             return
         }
         
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed();
+            return;
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
+        previewLayer.frame = self.view.bounds
+        previewLayer.videoGravity = AVLayerVideoGravityResize;
+        self.view.layer.addSublayer(previewLayer);
+        
+        captureSession.startRunning();
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning();
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning();
+        }
     }
     
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        captureSession.stopRunning()
         
-        // Check if the metadataObjects array is not nil and it contains at least one object.
-        if metadataObjects == nil || metadataObjects.count == 0 {
-            qrCodeFrameView?.frame = CGRect.zero
-            messageLabel.backgroundColor = UIColor.lightGray
-            messageLabel.text = "Kein QR-Code erkannt!"
-            return
-        }
-        
-        // Get the metadata object.
-        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        
-        var valid:Bool = false
-        
-        //let editViewController: EditViewController = EditViewController(nibName: nil, bundle: nil)
-        
-        
-        // Here we use filter method to check if the type of metadataObj is supported
-        // Instead of hardcoding the AVMetadataObjectTypeQRCode, we check if the type
-        // can be found in the array of supported bar codes.
-        if supportedBarCodes.contains(metadataObj.type) {
-            //        if metadataObj.type == AVMetadataObjectTypeQRCode {
-            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
-            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
-            qrCodeFrameView?.frame = barCodeObject!.bounds
+        if let metadataObject = metadataObjects.first {
+            let readableObject = metadataObject as! AVMetadataMachineReadableCodeObject;
             
-            if metadataObj.stringValue != nil {
-                
-                
-                valid = DBViewController().validate(metadataObj.stringValue)
-                
-                if (valid == true){
-                    messageLabel.backgroundColor = UIColor.green
-                    messageLabel.text = "Ticket gültig!"
-                    
-                    /*
-                    
-                    let alert_valid = UIAlertController(title: "Ticket gültig!", message: "\(metadataObj.stringValue)", preferredStyle: UIAlertControllerStyle.alert)
-                    let backView = alert_valid.view.subviews.last?.subviews.last
-                    backView?.layer.cornerRadius = 10.0
-                    backView?.backgroundColor = UIColor.green
-                    alert_valid.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
-                    self.present(alert_valid, animated: true, completion: nil)
- 
-                     */
-                
-                
-                    
-                }else{
-                    messageLabel.backgroundColor = UIColor.red
-                    messageLabel.text = "Ticket ungültig!"
-                    
-                    /*
-                    let alert_invalid = UIAlertController(title: "Ticket ungültig!", message: "\(metadataObj.stringValue)", preferredStyle: UIAlertControllerStyle.alert)
-                    let backView = alert_invalid.view.subviews.last?.subviews.last
-                    backView?.layer.cornerRadius = 10.0
-                    backView?.backgroundColor = UIColor.red
-                    alert_invalid.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
-                    self.present(alert_invalid, animated: true, completion: nil)
-                    */
-                    }
- 
-                }
-            }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: readableObject.stringValue);
         }
+        
+        //dismiss(animated: true)
+    }
+    
+    func found(code: String) {
+        print(code)
+        displaycode.backgroundColor = UIColor.green
+        displaycode.text = code
+        
+        let attendee = "Alexander Seitz"
+        
+        
+        if (code == "JSKDJAKSJ"){
+        
+            let alertController = UIAlertController(title: "GÜLTIG", message: "Gast: \(attendee)", preferredStyle: .alert)
+            
+            let cancelAction = UIAlertAction(title: "einchecken", style: .cancel) { action in
+                print(action)
+                self.captureSession.startRunning()
+            }
+            alertController.addAction(cancelAction)
+            
+            let destroyAction = UIAlertAction(title: "abbrechen", style: .destructive) { action in
+                print(action)
+                self.captureSession.startRunning()
+            }
+            alertController.addAction(destroyAction)
+            
+            self.present(alertController, animated: true) {
+                // ...
+            }
+            
+        }else{
+            
+            let alertController = UIAlertController(title: "UNGÜLTIG", message: "Das Ticket ist nicht gültig", preferredStyle: .alert)
+            
+            
+            let destroyAction = UIAlertAction(title: "abbrechen", style: .destructive) { action in
+                print(action)
+                self.captureSession.startRunning()
+            }
+            alertController.addAction(destroyAction)
+            
+            self.present(alertController, animated: true) {
+                // ...
+            }
+            
+    }
+        
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+   
     
     func toggleFlash() {
         let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
